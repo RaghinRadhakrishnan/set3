41a08e06a001b0691f7a2430ac0017f1afe5b72d
RaghinRadhakrishnan <30748414+RaghinRadhakrishnan@users.noreply.github.com>
2018-02-02 16:39:50 +0530

    Create index.c

---
+++ index.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int a[10],i,n;
+printf("Enter the number");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+printf("%d\t%d\n",a[i],i);
+}
+}
